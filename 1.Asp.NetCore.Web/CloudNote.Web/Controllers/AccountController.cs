0646ae3322c29fc88aa8a788508553f0e7bf7c9e
ASUS <ASUS@OASIS-DP-GUOXC>
2021-03-23 19:07:52 +0800

    add ztree,bootstrap,vue css,js

---
+++ 1.Asp.NetCore.Web/CloudNote.Web/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CloudNote.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        public IActionResult SignOut()
+        {
+            return View("SignIn");
+        }
+
+        public IActionResult Regist()
+        {
+            return View();
+        }
+
+    }
+}
